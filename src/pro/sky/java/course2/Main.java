dbd936012993fad6ddc155b69765516faad60eba
pro100 <pro100tkach@mail.ru>
2021-11-07 10:13:03 +0300

    Initial commit

---
+++ src/pro/sky/java/course2/Main.java
@@ -0,0 +1,17 @@
+package pro.sky.java.course2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 121;
+        int b;
+        b = (a / 10) - (a / 100 * 10);
+
+        System.out.println(" B = " + b );
+
+
+        // write your code here
+    }
+
+
+}
